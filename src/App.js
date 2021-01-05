7b7b2ad74ab01665f73f0595b305630f847c76ff
sanddy-man <guardian.sanddy@gmail.com>
2021-01-05 15:35:47 +0800

    pull-to-refresh feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useRef, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
+import PullToRefresh from 'react-simple-pull-to-refresh';
 import './App.css';
 
 function App() {
@@ -36,6 +37,7 @@ function App() {
     dataLoading.current = true;
     try {
       const res = await axios.get(`http://api.unsplash.com/photos?page=${pageNumRef.current}&client_id=6c446b49b72a4c559d9b9d67183d5c1de1981d16f309063c3b994086e6ce1a26`);
+      pageNumRef.current++;
       setData([...res.data]);
       dataLoading.current = false;
       resolve();
@@ -46,23 +48,23 @@ function App() {
   }
 
   return (
-    <InfiniteScroll
-      pageStart={0}
-      loadMore={loadFunc}
-      hasMore={true}
-      loader={<div className="loader" key={0}>Loading ...</div>}
-    >
-      {data.map(item => <div key={item.id} className="align-center">
-        {/* <img src={item.urls.small} alt={item.alt_description} />
-        <div>{item.alt_description}</div> */}
-        <div className="demo-card-square mdl-card mdl-shadow--2dp margin-top">
-          <img className="mdl-card__title mdl-card--expand" src={item.urls.small} alt={item.alt_description} />
-          <div className="mdl-card__supporting-text">
-            {item.alt_description}
+    <PullToRefresh onRefresh={handleRefresh} canFetchMore={false}>
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={loadFunc}
+        hasMore={true}
+        loader={<div className="loader" key={0}>Loading ...</div>}
+      >
+        {data.map(item => <div key={item.id} className="align-center">
+          <div className="demo-card-square mdl-card mdl-shadow--2dp margin-top">
+            <img className="mdl-card__title mdl-card--expand" src={item.urls.small} alt={item.alt_description} />
+            <div className="mdl-card__supporting-text">
+              {item.alt_description}
+            </div>
           </div>
-        </div>
-      </div>)}
-    </InfiniteScroll>
+        </div>)}
+      </InfiniteScroll>
+    </PullToRefresh>
   );
 }
 
